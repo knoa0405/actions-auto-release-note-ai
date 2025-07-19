9a04ca58ecae88f73e164314e3f516a87eb5a898
강노아 <knoa0405@gmail.com>
2025-07-20 01:30:25 +0900

    refactor: remove unused test function and clean up console logs in coloso-intl/index.js

--- coloso-intl/index.js
+++ coloso-intl/index.js
@@ -1,9 +1,3 @@
 console.log("test");
 console.log("test2");
 console.log("test4");
-
-const test = () => {
-  console.log("test");
-};
-
-test();
--- coloso-jp/index.js
+++ coloso-jp/index.js
@@ -1,2 +1,8 @@
 console.log("test");
 console.log("test");
+
+const test = () => {
+  console.log("test");
+};
+
+test();
